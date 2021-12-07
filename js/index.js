06c37cf9b314fe2c3cc9587aaa2c70eace9f2a49
ChelseyG <86028043+ChelseyGravel@users.noreply.github.com>
2021-12-07 00:25:09 -0500

    Add

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
+$("#myBrand").on("click", function(){
+  $("").fadeToggle("slow");
+})
 console.log("your indes.js file is loaded correctly!");
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
